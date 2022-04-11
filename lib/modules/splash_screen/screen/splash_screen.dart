8a5bc90c938ea5236837dcfbaed51915ac9bceba
Harsh Chudasma <65327841+HarshChudasma@users.noreply.github.com>
2022-04-11 23:17:14 +0530

    splash screen and widget test commit by harsh chudasma 11-04-2022

--- lib/modules/splash_screen/screen/splash_screen.dart
+++ lib/modules/splash_screen/screen/splash_screen.dart
@@ -10,6 +10,10 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: Scaffold(
+        body: Container(),
+      ),
+    );
   }
 }
