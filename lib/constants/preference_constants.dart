afd267f660f2fbb028da69327e90f6018798c524
Harsh Chudasma <65327841+HarshChudasma@users.noreply.github.com>
2022-04-17 22:17:50 +0530

    home screen commit by harsh chudasma 17-04-2022

---
+++ lib/constants/preference_constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+
+class ColorCommonConstants {
+  static const tealColor = Color(0xFF00E676);
+  static const brownColor = Color(0xFF5D4037);
+  static const greyColor = Color(0xFF424242);
+  static const blueGrayColor = Color(0xFF424242);
+  static const lightLemonColor = Color(0xFFDCE775);
+}
--- lib/modules/home_page/screen/home_screen.dart
+++ lib/modules/home_page/screen/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce_project/constants/preference_constants.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -12,28 +13,49 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Column(
-          children: [
-            Container(
-              child: Row(
-                children: [
-                  Column(
-                    children: const [],
-                  ),
-                  Container(
-                    width: 45.0,
-                    height: 45.0,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(
-                        15.0,
-                      ),
-                      color: Colors.blueGrey,
+        body: Container(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      children: const [
+                        Text(
+                          "Country",
+                          style: TextStyle(
+                            color: ColorCommonConstants.brownColor,
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          "City",
+                          style: TextStyle(
+                            color: ColorCommonConstants.greyColor,
+                            fontSize: 18,
+                          ),
+                        ),
+                      ],
                     ),
-                  ),
-                ],
+                    Container(
+                      width: 45.0,
+                      height: 45.0,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(
+                          15.0,
+                        ),
+                        color: Colors.blueGrey,
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
